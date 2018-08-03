415d06628bf97a80a620ee81f20f0dfc15828482
danielzcoelho <danielzcoelho@gmail.com>
2018-08-02 22:33:34 -0300

    Rendering templates with handlebars ( Partials and Helper )

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  //res.send('<h1>Hello Express!</h1>');
+hbs.registerHelper('getCurrentYear', () => {
 
-  res.send({
-    name: 'Daniel Coelho',
-    hobbies: ['Beers', 'Dogs']
+  return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+
+  res.render('home.hbs', {
+    pageTitle: 'Homepage',
+    welcomeMsg: 'Find out how exciting is Node.js programming'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
+
   res.send({
     errorMessage: 'Unable to get the url'
   });
